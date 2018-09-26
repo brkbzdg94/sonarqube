6d404f0605ccc72018c1f89f588ca11ed1ddddec
Burak <cixnx@hotmail.com>
2018-09-26 09:35:45 +0300

    v2

--- SonarQube/SonarQube/Controllers/HomeController.cs
+++ SonarQube/SonarQube/Controllers/HomeController.cs
@@ -26,5 +26,14 @@ namespace SonarQube.Controllers
 
             return View();
         }
+
+        public void sonar()
+        {
+            int target = -5;
+            int num = 3;
+
+            target = -num;  // Noncompliant; target = -3. Is that really what's meant?
+            target = +num; // Noncompliant; target = 3
+        }
     }
 }
