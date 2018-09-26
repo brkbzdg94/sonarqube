a0be2d5003b0bd726440945c9c342d50919d2f57
Burak <cixnx@hotmail.com>
2018-09-26 09:22:57 +0300

    v1

---
+++ SonarQube/SonarQube/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SonarQube.MvcApplication" Language="C#" %>
---
+++ SonarQube/SonarQube/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SonarQube.Startup))]
+namespace SonarQube
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
